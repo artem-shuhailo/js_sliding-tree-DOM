1c2d8c76339573e03fbe14cc385b80440e5bcc02
Artem Shuhailo <artem0462@gmail.com>
2023-12-13 12:11:00 +0200

    make build

---
+++ index.8a8d58b7.js
@@ -0,0 +1,3 @@
+var e=document.querySelector(".tree");e.querySelectorAll("li").forEach(function(e){var n=document.createElement("span");e.prepend(n),n.append(n.nextSibling)}),e.addEventListener("click",function(e){var n=e.target.nextSibling;"none"===n.style.display?n.style.display="block":n.style.display="none"});//# sourceMappingURL=index.8a8d58b7.js.map
+
+//# sourceMappingURL=index.8a8d58b7.js.map
---
+++ index.f75de5e1.js
@@ -0,0 +1,15 @@
+"use strict";
+const tree = document.querySelector(".tree");
+const treeItems = tree.querySelectorAll("li");
+treeItems.forEach((item)=>{
+    const span = document.createElement("span");
+    item.prepend(span);
+    span.append(span.nextSibling);
+});
+tree.addEventListener("click", (e)=>{
+    const item = e.target.nextSibling;
+    if (item.style.display === "none") item.style.display = "block";
+    else item.style.display = "none";
+});
+
+//# sourceMappingURL=index.f75de5e1.js.map
